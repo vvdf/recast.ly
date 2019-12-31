ee7bcae57c872eced7d26e1db9cba3226e8f3150
vvdf <t42please@gmail.com>
2019-12-30 20:52:11 -0800

    Implement setCurrent video function, passed to video list children

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,23 +3,42 @@ import VideoPlayer from './VideoPlayer.js';
 import VideoList from './VideoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <Search />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentVideo: exampleVideoData[0],
+      videos: exampleVideoData,
+    };
+    this.setCurrent = this.setCurrent.bind(this);
+  }
+
+  setCurrent(video) {
+    this.setState({
+      currentVideo: video
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search />
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.currentVideo}/>
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videos} setCurrent={this.setCurrent}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video={exampleVideoData[0]}/>
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={exampleVideoData}/>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
