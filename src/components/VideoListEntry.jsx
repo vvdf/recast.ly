85610b449bff7e502f69cb61d57e33288a4a5d7c
vvdf <t42please@gmail.com>
2019-12-30 19:46:21 -0800

    Add dynamic thumbnail assignment from video

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,7 +1,7 @@
 var VideoListEntry = (props) => (
   <div className="video-list-entry media">
     <div className="media-left media-middle">
-      <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
+      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
     </div>
     <div className="media-body">
       <div className="video-list-entry-title">{props.video.snippet.title}</div>
